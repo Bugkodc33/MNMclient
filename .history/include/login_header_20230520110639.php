d1fd5adc0d8c3d1993e90e4d3ae0568f0df3ae78
w3schools-test <test@w3schools.com>
2023-05-20 14:33:36 +0700

    update icon

--- .history/include/login_header_20230520110639.php
+++ .history/include/login_header_20230520110639.php
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Hệ Thống Quản Lí Bác Sĩ</title>
+  <title>Employee time Management</title>
   <meta charset="utf-8">
-  <link rel="shortcut icon" href="https://media.istockphoto.com/id/1470544119/vi/anh/n%E1%BB%AF-chuy%C3%AAn-gia-cntt.jpg?s=612x612&w=is&k=20&c=JlHtKUPuwbF7I8yXZPhR1CoLtzesI5-4D5gzFgfelzo=" type="image/x-icon">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="assets/css/bootstrap.min.css">
   <link rel="stylesheet" href="assets/css/bootstrap.theme.min.css">
--- .history/include/login_header_20230520142932.php
+++ .history/include/login_header_20230520142932.php
@@ -3,7 +3,7 @@
 <head>
   <title>Hệ Thống Quản Lí Bác Sĩ</title>
   <meta charset="utf-8">
-  <link rel="shortcut icon" href="https://media.istockphoto.com/id/1470544119/vi/anh/n%E1%BB%AF-chuy%C3%AAn-gia-cntt.jpg?s=612x612&w=is&k=20&c=JlHtKUPuwbF7I8yXZPhR1CoLtzesI5-4D5gzFgfelzo=" type="image/x-icon">
+  <link rel="shortcut icon" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css" type="image/x-icon">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="assets/css/bootstrap.min.css">
   <link rel="stylesheet" href="assets/css/bootstrap.theme.min.css">

04de66ddcf2e077831f3e1a94d85db6ce98e6a57
Ducdevspdn <ductrantad23@gmail.com>
2023-06-08 07:35:00 +0700

    connect php PDO

--- .history/classes/connection_20230608073355.php
+++ .history/classes/connection_20230608073355.php
@@ -3,6 +3,16 @@ $servername = "localhost";
 $username = "root"; 
 $password = ""; 
 $database = "etmsh"; 
+
+// $conn = new mysqli($servername, $username, $password, $database);
+
+// if ($conn->connect_error) {
+//     die("Kết nối không thành công: " . $conn->connect_error);
+// }
+//  else
+// {
+//     echo "Kết nối thành công";
+// }
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
